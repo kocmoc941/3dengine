8c6c338771b61bd7416ff21cfc7fdaa19d25a4f1
kocmoc941 <kocmoc-sp@yandex.ru>
2016-06-09 17:11:29 +0300

    show window

--- Source/client/GLCreateWindow.lib/UTEST/caller/caller/calldll.cpp
+++ Source/client/GLCreateWindow.lib/UTEST/caller/caller/calldll.cpp
@@ -4,6 +4,34 @@
 
 char DLL[1024]{};
 
+LRESULT CALLBACK WindowProc(
+  _In_ HWND   hwnd,
+  _In_ UINT   uMsg,
+  _In_ WPARAM wParam,
+  _In_ LPARAM lParam
+  ) {
+  switch (uMsg) {
+    case WM_KEYDOWN:
+      if (wParam == VK_ESCAPE) {
+        PostQuitMessage(0);
+      }
+      break;
+    case WM_PAINT:
+      HDC hdc;
+      PAINTSTRUCT lp;
+      hdc = BeginPaint(hwnd, &lp);
+      HBRUSH br = CreateSolidBrush(RGB(0,0,0));
+      SelectObject(hdc, br);
+      RECT r;
+      GetWindowRect(hwnd, &r);
+      Rectangle(hdc, 0,0, r.right, r.bottom);
+      DeleteObject(br);
+      EndPaint(hwnd, &lp);
+      break;
+  }
+  return DefWindowProcA(hwnd, uMsg, wParam, lParam);
+}
+
 int main(int argc, char** argv) {
   if (argc < 4)
   {
@@ -15,22 +43,30 @@ int main(int argc, char** argv) {
   if (att != INVALID_FILE_ATTRIBUTES) {
     HMODULE h = LoadLibraryA(DLL);
     if (h) {
-      HWND(*call)(int a, int b);
-      char func[1024]{};
-      strcpy(func, argv[2]);
-      call = (HWND(*)(int a, int b))GetProcAddress(h, func);
+      HWND(*call)(WNDPROC);
+      call = (HWND(*)(WNDPROC))GetProcAddress(h, argv[2]);
       if (call) {
-        std::cout << "returned: " << (int)call(10, 20);
+        HWND hwnd = call(WindowProc);
+        if (hwnd) {
+          ShowWindow(hwnd, SW_SHOW);
+          MSG msg;
+          while (GetMessageA(&msg, 0, 0, 0)) {
+            if (msg.message == WM_QUIT)
+              break;
+            TranslateMessage(&msg);
+            DispatchMessageA(&msg);
+          }
+        }
+        std::cout << "returned: " << hwnd;
         std::cout << "\n";
       }
       else
-        std::cout << argv[2] << " not found";
+        std::cout << argv[2] << " not found\n";
       FreeLibrary(h);
     }
     else
-      std::cout << "wrong structure dll";
+      std::cout << "wrong structure dll\n";
   }
   else
-    std::cout << "dll not found";
-  std::cout << "\n";
+    std::cout << "dll not found\n";
 }
--- Source/client/GLCreateWindow.lib/main.asm
+++ Source/client/GLCreateWindow.lib/main.asm
@@ -9,9 +9,14 @@ EXPORT GLCreateWindow
 segment .data use32 align=1
   cl_name db '__unique_class_name_cogl_creator__',0
   wn_name db '3D Engine',0
-  wnd_size equ dword wndsize-WNDCLASSEX
+  msgerr:
+    .register: db 'RegisterClassExA', 0
+    .window: db 'CreateWindowExA',0
+    
+  wnd_size equ dword WNDCLASSEX.wndsize
 
-struc WNDCLASSEX
+segment .bss use32 align=1
+WNDCLASSEX:
 	.cbSize resd 1
 	.cbStyle resd 1
 	.lpfnWndProc resd 1
@@ -22,45 +27,100 @@ struc WNDCLASSEX
 	.lpszMenuName resd 1
 	.lpszClassName resd 1
 	.hIconSm resd 1
-  wndsize:
-endstruc
+  .wndsize equ $-WNDCLASSEX
+  
+MSG:
+  .hwnd resd 1
+  .message resd 1
+  .wParam resd 1
+  .lParam resd 1
+  .time resd 1
+  .pt resd 1
   
 segment .text use32 align=1
-
 DLLMain:
-  xor eax, eax
-  sete al
-  ret 12
+  cmp [esp+8], dword 1 ; DLL_PROCESS_ATTACH
+  jne dllexit
+    mov eax, [esp+16]
+    mov [WNDCLASSEX.hInstance], eax
+  dllexit:
+    xor eax, eax
+    sete al
+    ret 12
 
-InitWnd:
+InitWndClass:
   push ebp
   mov ebp, esp
   mov eax, wnd_size
-  ;wndeax(cbSize)
-  mov [WNDCLASSEX.cbSize], dword 0
+  wndeax(cbSize)
   mov eax, 0x0002 ;CS_HREDRAW
   or eax, 0x0001  ;CS_VREDRAW
-  ;wndeax(cbStyle)
-  ;mov eax, [esp-8]
-  ;wndeax(lpfnWndProc)
+  wndeax(cbStyle)
+  mov eax, [esp+16] ; lpfnWndProc
+  wndeax(lpfnWndProc)
   xor eax, eax
-  ;wndeax(cbClsExtra)
-  ;wndeax(cbWndExtra)
-  ;wndeax(hInstance)
-  ;wndeax(hIcCurBr)
-  ;wndeax(lpszMenuName)
-  ;wndeax(lpszMenuName)
+  wndeax(cbClsExtra)
+  wndeax(cbWndExtra)
+;  wndeax(hInstance) ; in DLLMain
+  wndeax(hIcCurBr)
+  wndeax(lpszMenuName)
+  wndeax(lpszMenuName)
   mov eax, cl_name
-  ;wndeax(lpszClassName)
+  wndeax(lpszClassName)
   xor eax, eax
-  ;wndeax(hIconSm)
+  wndeax(hIconSm)
   leave
   ret
   
-GLCreateWindow:
+GLCreateWindow: ; one param - lpfnWndProc
   push ebp
   mov ebp, esp
-  ;push 0
-  call InitWnd
-  leave
-  ret
+  call InitWndClass
+  push WNDCLASSEX
+  call [RegisterClassExA]
+  test eax,eax
+  jz error.reg
+
+  push 0 ; lpparam
+  push dword [WNDCLASSEX.hInstance]
+  push 0 ; menu
+  push 0 ; parent
+  push 768 ; height
+  push 1024 ; width
+  push 10 ; y
+  push 10 ; x
+  mov eax, 0x00000000 ; overlaooed
+  push eax ; style
+  push wn_name
+  push cl_name
+  mov eax, 00000200h ; clientedge
+  or eax, 00000100h ; windowdedge
+  push eax ; dwexstyle
+  call [CreateWindowExA]
+  test eax, eax
+  jz error.wnd
+
+  exit:
+    leave  
+    ret
+
+  error:
+    .reg:
+      push cl_name
+      push WNDCLASSEX.hInstance
+      call [UnregisterClassA]
+      push 0
+      push 0
+      push msgerr.register
+      push 0
+      jmp .msgbox
+    .wnd:
+      push 0
+      push 0      
+      push msgerr.window
+      push 0
+      jmp .msgbox
+    .msgbox:
+      call [MessageBoxA]
+      call [GetLastError]
+    jmp exit
